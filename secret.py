e6c9a6f0133028527cce942891b9bda9bb26f052
tdonadello <146837755+tdonadello@users.noreply.github.com>
2025-03-19 09:55:38 +0100

    Update secret.py
    Odevzdání testu

--- secret.py
+++ secret.py
@@ -1,22 +1,30 @@
 from playwright.sync_api import sync_playwright
 import os
 
-
+login = "Jarmil"
+password = "Admin123"
 
 
 def main():
 
-    # with sync_playwright() as p:
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
 
-        # browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
-        # !!!
-        # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
-        # !!!
+        page.goto("https://js-trebesin.github.io/playwright-exam/")
 
+        page.fill('input[id="login"]', login)
+        page.fill('input[id="pass"]', password)
 
-        # browser.close()
+        page.click('button[class="login-btn"]')
+        zprava = page.locator('div[class="psst"]').text_content() 
+        print(zprava)
+        
+
+        input("Klikni na cokoliv pro zavření prohlížeče")
+        browser.close()
     
 
 if __name__ == "__main__":
-    main()
+    main())
