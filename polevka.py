302a65ff0e6c59972296eb1777f19e3406c75995
tdonadello <146837755+tdonadello@users.noreply.github.com>
2025-03-19 09:56:09 +0100

    Update polevka.py
    Odevzdání testu

--- polevka.py
+++ polevka.py
@@ -4,10 +4,15 @@ import requests
 
 def main():
 
+    url = "https://js-trebesin.github.io/bsoup-exam/"
+    response = requests.get(url)
 
-    # response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser") # <--- Úkol: popiš krátce, co tohle dělá = umožní nám hledat html prvky a pracovat s nimi
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    ingredience = soup.find(style="z-index")
+    print(f"Ingredience pro češnečku jsou ({ingredience.text})")
+
+
 
 
 if __name__ == "__main__":
